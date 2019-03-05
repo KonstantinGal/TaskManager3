41178c0344770572a0c663e1e14162fa7dd2d080
Konstantin Galkin <galkink@my.erau.edu>
2019-03-05 08:33:30 -0500

    Add project files.

---
+++ TaskManager3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaskManager3.Startup))]
+namespace TaskManager3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
